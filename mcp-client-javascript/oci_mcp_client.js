2fb87b11db434ca5c12f6e0408f3856657bf025d
Marc Gueury <marc.gueury@oracle.com>
2025-10-17 18:57:03 +0200

    v3.0

--- mcp-client-javascript/oci_mcp_client.js
+++ mcp-client-javascript/oci_mcp_client.js
@@ -10,92 +10,81 @@ import { StdioClientTransport } from "@modelcontextprotocol/sdk/client/stdio.js"
 import { StreamableHTTPClientTransport } from "@modelcontextprotocol/sdk/client/streamableHttp.js";
 import readline from "readline/promises";
 
-const servingMode: models.OnDemandServingMode = {
-    modelId: env.TF_VAR_genai_cohere_model,
-    servingType: "ON_DEMAND",
+const servingMode = {
+  modelId: env.TF_VAR_genai_cohere_model,
+  servingType: "ON_DEMAND",
 };
 
 class MCPClient {
-  private mcp: Client;
-  private llm: GenerativeAiInferenceClient = null;
-  private transport: StdioClientTransport | null = null;
-  private tools: Object[] = [];
-
   constructor() {
     this.mcp = new Client({ name: "mcp-client-cli", version: "1.0.0" });
+    this.llm = null;
+    this.transport = null;
+    this.tools = [];
   }
 
-  debug( s: string ) {
-    // console.log( s ); 
+  debug(s) {
+    // console.log(s);
   }
 
   async initLLM() {
     const provider = await new InstancePrincipalsAuthenticationDetailsProviderBuilder().build();
     this.llm = new GenerativeAiInferenceClient({
-        authenticationDetailsProvider: provider,
-      }
-    );
-    this.llm.endpoint = "https://inference.generativeai."+env.TF_VAR_region+".oci.oraclecloud.com";
+      authenticationDetailsProvider: provider,
+    });
+    this.llm.endpoint = "https://inference.generativeai." + env.TF_VAR_region + ".oci.oraclecloud.com";
   }
 
-  async connectToServer(serverPath: string) {
-    /**
-     * Connect to an MCP server
-     *
-     * @param serverPath - Path to the server script URL (Streamable HTTP) or STDIO (.py or .js)
-     */
+  async connectToServer(serverPath) {
     try {
-      if ( serverPath.startsWith('http') ) {
+      if (serverPath.startsWith('http')) {
         const url = new URL(serverPath);
         this.transport = new StreamableHTTPClientTransport(url);
         await this.mcp.connect(this.transport);
       } else {
-        // Determine script type and appropriate command
         const isJs = serverPath.endsWith(".js");
         const isPy = serverPath.endsWith(".py");
         if (!isJs && !isPy) {
-            throw new Error("Server script must be a .js or .py file");
+          throw new Error("Server script must be a .js or .py file");
         }
         const command = isPy
-            ? process.platform === "win32"
+          ? process.platform === "win32"
             ? "python"
             : "python3.12"
-            : process.execPath;
+          : process.execPath;
 
-        // Initialize transport and connect to server
         this.transport = new StdioClientTransport({
-            command,
-            args: [serverPath],
+          command,
+          args: [serverPath],
         });
         await this.mcp.connect(this.transport);
       }
-      this.debug( "before ListTools" ); 
+      this.debug("before ListTools");
       await new Promise(r => setTimeout(r, 2000));
 
-      // List available tools
       const toolsResult = await this.mcp.listTools();
 
-      this.debug( "toolsResult " + JSON.stringify(toolsResult) );      
+      this.debug("toolsResult " + JSON.stringify(toolsResult));
       this.tools = toolsResult.tools.map((tool) => {
-        this.debug( "tool.inputSchema: " + JSON.stringify(tool.inputSchema) );  
+        this.debug("tool.inputSchema: " + JSON.stringify(tool.inputSchema));
         var tool_schema = tool.inputSchema.properties;
-        this.debug( "tool_schema: " + JSON.stringify(tool_schema) );  
+        this.debug("tool_schema: " + JSON.stringify(tool_schema));
         var params = {}
-        Object.keys(tool_schema).forEach(function(key,index) {
-            params[key] = {
-              type: tool_schema[key].type,
-              description: tool_schema[key].name,
-              isRequired: false
-            }
+        Object.keys(tool_schema).forEach(function(key, index) {
+          params[key] = {
+            type: tool_schema[key].type,
+            description: tool_schema[key].name,
+            isRequired: false
+          }
         });
-        this.debug( "tool.inputSchema " + JSON.stringify(params) );      
+        this.debug("tool.inputSchema " + JSON.stringify(params));
         return {
           name: tool.name,
           description: tool.description,
           parameterDefinitions: params,
         };
       });
-      this.debug( "this.tools: " + JSON.stringify(this.tools) );  
+      this.debug("this.tools: " + JSON.stringify(this.tools));
       console.log(
         "Connected to server with tools:",
         this.tools.map(({ name }) => name),
@@ -106,50 +95,41 @@ class MCPClient {
     }
   }
 
-  async processQuery(query: string) {
-    /**
-     * Process a query using Claude and available tools
-     *
-     * @param query - The user's input query
-     * @returns Processed response as a string
-     */
-    // Chat Details
-    const chatRequest: requests.ChatRequest = {
-        chatDetails: {
-            compartmentId: env.TF_VAR_compartment_ocid,
-            servingMode: servingMode,
-            chatRequest: {
-                message: query,
-                apiFormat: "COHERE",
-                maxTokens: 2000,
-                temperature: 0,
-                tools: this.tools,
-            }
-        },
-        retryConfiguration: NoRetryConfigurationDetails
+  async processQuery(query) {
+    const chatRequest = {
+      chatDetails: {
+        compartmentId: env.TF_VAR_compartment_ocid,
+        servingMode: servingMode,
+        chatRequest: {
+          message: query,
+          apiFormat: "COHERE",
+          maxTokens: 2000,
+          temperature: 0,
+          tools: this.tools,
+        }
+      },
+      retryConfiguration: NoRetryConfigurationDetails
     };
-    this.debug( "chatRequest: " + JSON.stringify(chatRequest) );    
+    this.debug("chatRequest: " + JSON.stringify(chatRequest));
     const response = await this.llm.chat(chatRequest);
     const chatResponse = response.chatResult.chatResponse;
-    const messages: Object[] = [
+    const messages = [
       {
         role: "user",
         content: query,
       },
-    ];    
-    this.debug( "chatResponse: " + JSON.stringify(chatResponse) );   
+    ];
+    this.debug("chatResponse: " + JSON.stringify(chatResponse));
 
-    // Process response and handle tool calls
     const finalText = [];
 
     finalText.push(chatResponse.text);
 
-    if ( chatResponse.toolCalls ) {
-      for (const toolCall of chatResponse.toolCalls) { 
-        console.log(toolCall); 
-        // Execute tool call
+    if (chatResponse.toolCalls) {
+      for (const toolCall of chatResponse.toolCalls) {
+        console.log(toolCall);
         const toolName = toolCall.name;
-        const toolArgs = toolCall.parameters as { [x: string]: unknown } | undefined;
+        const toolArgs = toolCall.parameters;
         finalText.push(
           `[Calling tool ${toolName} with args ${JSON.stringify(toolArgs)}]`,
         );
@@ -157,15 +137,14 @@ class MCPClient {
           name: toolName,
           arguments: toolArgs,
         });
-        this.debug( "result: " + JSON.stringify(result) );           
+        this.debug("result: " + JSON.stringify(result));
         finalText.push(`[Calling tool done]`);
         finalText.push(result.content[0].text);
-        this.debug( "result: " + result.content[0].text );    
+        this.debug("result: " + result.content[0].text);
 
-        // Continue conversation with tool results
         messages.push({
           role: "user",
-          content: result.content[0].text as string,
+          content: result.content[0].text,
         });
       }
     }
@@ -174,9 +153,6 @@ class MCPClient {
   }
 
   async chatLoop() {
-    /**
-     * Run an interactive chat loop
-     */
     const rl = readline.createInterface({
       input: process.stdin,
       output: process.stdout,
@@ -191,7 +167,7 @@ class MCPClient {
         if (message.toLowerCase() === "quit") {
           break;
         }
-        console.log("\n" + message);        
+        console.log("\n" + message);
         const response = await this.processQuery(message);
         console.log("\n" + response);
       }
@@ -204,9 +180,6 @@ class MCPClient {
   }
 
   async cleanup() {
-    /**
-     * Clean up resources
-     */
     await this.mcp.close();
   }
 }
---
+++ mcp-client-javascript/oci_mcp_client.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+cd $SCRIPT_DIR
+
+. ../env.sh
+node oci_mcp_client.js ../python-fastmcp/mcp_add.py

8a3c61655fbe1a1ed23b331d364e11499f6291a4
Marc Gueury <marc.gueury@oracle.com>
2025-10-20 11:07:18 +0200

    v3.0

--- mcp-client-javascript/oci_mcp_client_main.js
+++ mcp-client-javascript/oci_mcp_client_main.js
@@ -1,4 +1,4 @@
-const oci_mcp_client = require("oci_mcp_client_lib");
+const oci_mcp_client = require("./oci_mcp_client_lib");
 
 const mcpClient = new oci_mcp_client.MCPClient();
 mcpClient.main();
